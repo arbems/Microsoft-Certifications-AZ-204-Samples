5080c379f2e753475e190fd7abe8433a75914c86
arbems <arbems@outlook.com>
2023-12-22 21:02:41 +0100

    Add UDF

--- src/8.x/Azure Cosmos DB/CosmosUserDefinedFunctions/Program.cs
+++ src/8.x/Azure Cosmos DB/CosmosUserDefinedFunctions/Program.cs
@@ -1,1 +1,56 @@
-﻿
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Scripts;
+using System.Configuration;
+
+string EndpointUri = ConfigurationManager.AppSettings["EndPointUri"]!;
+string PrimaryKey = ConfigurationManager.AppSettings["PrimaryKey"]!;
+
+CosmosClient cosmosClient;
+
+Container container = null!;
+
+string databaseId = "TodoDatabase";
+string containerId = "TodoItemContainer";
+
+await GetStartedDemoAsync();
+
+async Task GetStartedDemoAsync()
+{
+    // Create a new instance of the Cosmos Client
+    cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+
+    string udfId = "isOverdue";
+    await RegisterAUserDefinedFunction(udfId);
+    await GetItemQueryAsync();
+}
+
+async Task GetItemQueryAsync()
+{
+    container = cosmosClient.GetContainer(databaseId, containerId);
+
+    var iterator = container.GetItemQueryIterator<dynamic>(
+        "SELECT c.id, c.Title, udf.isOverdue(c) AS IsOverdue FROM c WHERE udf.isOverdue(c) = true");
+
+    while (iterator.HasMoreResults)
+    {
+        var results = await iterator.ReadNextAsync();
+        foreach (var result in results)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
+
+async Task RegisterAUserDefinedFunction(string udfId)
+{
+    container = cosmosClient.GetContainer(databaseId, containerId);
+
+    UserDefinedFunctionResponse spResponse =
+        await container.Scripts.CreateUserDefinedFunctionAsync(new UserDefinedFunctionProperties
+        {
+            Id = udfId,
+            Body = File.ReadAllText(Path.Combine(AppContext.BaseDirectory, $@"js\{udfId}.js"))
+        });
+
+    Console.WriteLine("Created UDF in database with id: {0} Operation consumed {1} RUs.\n", spResponse.Resource.Id, spResponse.RequestCharge);
+}
---
+++ src/8.x/Azure Cosmos DB/CosmosUserDefinedFunctions/js/isOverdue.js
@@ -0,0 +1,10 @@
+﻿function isOverdue(item) {
+    if (!item.Reminder) {
+        return false;
+    }
+
+    var reminderDate = new Date(item.Reminder);
+    var currentDate = new Date();
+
+    return currentDate > reminderDate && !item.Done;
+}
