bc5a78085371ae8fd399490645ab7b933a53c262
arbems <arbems@outlook.com>
2023-12-22 19:46:47 +0100

    Add CosmosStoreProcedure  2

---
+++ src/8.x/Azure Cosmos DB/CosmosStoreProcedure/js/spCreateToDoItem.js
@@ -0,0 +1,13 @@
+﻿function spCreateToDoItem(item) {
+    var context = getContext();
+    var collection = context.getCollection();
+    var accepted = collection.createDocument(
+        collection.getSelfLink(),
+        item,
+        function (err, newitem) {
+            if (err) throw new Error('Error' + err.message);
+            context.getResponse().setBody(newitem);
+        }
+    );
+    if (!accepted) return;
+}
---
+++ src/8.x/Azure Cosmos DB/CosmosStoreProcedure/js/spDeleteToDoItems.js
@@ -0,0 +1,43 @@
+﻿function spDeleteToDoItems(query) {
+    var collection = getContext().getCollection();
+    var collectionLink = collection.getSelfLink();
+    var response = getContext().getResponse();
+    var responseBody = {
+        deleted: 0,
+        continuation: true
+    };
+    if (!query) throw new Error("The query is undefined or null.");
+    tryQueryAndDelete();
+    function tryQueryAndDelete(continuation) {
+        var requestOptions = { continuation: continuation };
+        var isAccepted = collection.queryDocuments(collectionLink, query, requestOptions, function (err, retrievedDocs, responseOptions) {
+            if (err) throw err;
+            if (retrievedDocs.length > 0) {
+                tryDelete(retrievedDocs);
+            } else if (responseOptions.continuation) {
+                tryQueryAndDelete(responseOptions.continuation);
+            } else {
+                responseBody.continuation = false;
+                response.setBody(responseBody);
+            }
+        });
+        if (!isAccepted) {
+            response.setBody(responseBody);
+        }
+    }
+    function tryDelete(documents) {
+        if (documents.length > 0) {
+            var isAccepted = collection.deleteDocument(documents[0]._self, {}, function (err, responseOptions) {
+                if (err) throw err;
+                responseBody.deleted++;
+                documents.shift();
+                tryDelete(documents);
+            });
+            if (!isAccepted) {
+                response.setBody(responseBody);
+            }
+        } else {
+            tryQueryAndDelete();
+        }
+    }
+}
